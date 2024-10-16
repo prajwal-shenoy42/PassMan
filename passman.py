1445d4bfe66f5e9f3a52e22f1e0accd4a21ce7ad
Prajwal <prajwalslocker15@gmail.com>
2024-10-16 15:51:29 +0530

    Completed Arguement Parser Code

--- passman.py
+++ passman.py
@@ -1,9 +1,24 @@
 import psycopg2
 import qryhandler
+import argparse
 
 conn = psycopg2.connect(database="postgres", user="postgres", password="postgres", host="localhost", port=5432)
 cursor = conn.cursor()
 
-qryhandler.fetch_secret(cursor, "google.com")
+parser = argparse.ArgumentParser(description="ArgumentParser for Passman")
+parser.add_argument("-a", "--add", help="add entry into passman", action="store_true")
+parser.add_argument("-d", "--delete", help="remove entry from passman", action="store_true")
+parser.add_argument("-f", "--fetch", help="fetch entry from passman", action="store_true")
+parser.add_argument("-u", "--url", nargs=1, help="url of the website")
+parser.add_argument("-n", "--username", nargs=1, help="username for the given url")
+
+
+args = parser.parse_args()
+if args.add:
+    qryhandler.insert_secret(cursor, conn, args.url[0], args.username[0], "")
+elif args.delete:
+    qryhandler.delete_secret(cursor, conn, args.url[0], args.username[0])
+elif args.fetch:
+    qryhandler.fetch_secret(cursor, args.url[0])
 
 conn.close()
